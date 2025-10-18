12773046166c2bf83c5def4025983c034ca090b9
nuclearBulldog <dellowcaleb@outlook.com>
2025-10-18 23:58:30 +1300

    Automated commit

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -28,16 +28,6 @@
           chrissicool/zsh-256color
           z-shell/zsh-fancy-completions
         ''
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-          name = "powerlevel10k-config";
-          src = ./p10k-config;
-          file = "p10k.zsh";
-        }
       ];
     };
   };
