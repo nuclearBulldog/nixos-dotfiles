98d05536d33f6b07876086f8666c1737eda5a89c
nuclearBulldog <dellowcaleb@outlook.com>
2025-10-18 22:53:29 +1300

    Automated commit

--- flake.nix
+++ flake.nix
@@ -1,28 +1,30 @@
 {
-    description = "Calebs Nixos";
+  description = "Calebs Nixos";
 
-    inputs = { 
-        nixpkgs.url = "nixpkgs/nixos-25.05";
-        home-manager = {
-            url = "github:nix-community/home-manager/release-25.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    #nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    outputs = { self, nixpkgs, home-manager, ... }: {
-        nixosConfigurations.cdell = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./configuration.nix
-                home-manager.nixosModules.home-manager
-                {
-                    home-manager = {
-                        useGlobalPkgs = true;
-                        useUserPackages = true;
-                        users.caleb = import ./home.nix;
-                        backupFileExtension = "backup";
-                    };
-                }
-            ];
-        };
+
+  };
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.cdell = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.caleb = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
     };
+  };
 }
---
+++ modules/packages.nix
@@ -0,0 +1,22 @@
+{ pkgs, inputs, ... }:
+
+let
+  inherit (builtins) attrValues;
+in
+
+{
+  environment.systemPackages = attrValues {
+    inherit
+      (pkgs)
+      libreoffice-fresh
+      clang
+      gnumake
+      cargo
+      go
+      gcc
+      onefetch
+      cpufetch
+      microfetch
+      ;
+  };
+}
