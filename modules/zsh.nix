91f743e947120afb6b3da4ad9f9682a86e5b27a3
nuclearBulldog <dellowcaleb@outlook.com>
2025-10-18 21:31:09 +1300

    Automated commit

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -14,13 +14,10 @@
           zsh-users/zsh-autosuggestions
           zsh-users/zsh-completions
           raisty/alt-and-select
-          Sam-programs/zsh-calc
-          GeoLMg/apt-zsh-plugin
           zuxfoucault/colored-man-pages_mod
           aemonge/zsh-hookie-projects
           chrissicool/zsh-256color
           z-shell/zsh-fancy-completions
-          zdharma-continuum/zui
           ohmyzsh/ohmyzsh path:lib/git.zsh
           ohmyzsh/ohmyzsh path:lib/pip.zsh
           ohmyzsh/ohmyzsh path:lib/command-not-found.zsh     
